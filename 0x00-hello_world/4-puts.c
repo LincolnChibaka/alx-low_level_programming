36fe7ac6c7f83b5ed823217b476a688019fb09df
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-11 04:39:31 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * Print the string "Programming is like building a multilingual puzzle" using puts function
+ * Then return 0 for a success
+ */
+#include <stdio.h>
+int main ()
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
