15812f794e7049f0aee0f527fb138de5a95b51ce
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-11 11:53:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -22,8 +22,7 @@ syscall(SYS_write, STDERR_FILENO, str, strlen(str));
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" \
-- Dora Korpar, 2013-10-19\n";
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2013-10-19\n";
 
 custom_print(message);
 return (1);
