1068083f99e9a71b83db60cdfabd641c32081c9d
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-11 09:01:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+
 /**
- * main-Print size of various types on the computer
+ * main - Prints the size of various types on the computer
  *
- * return- 0 for success
+ * Return: 0 for success
  */
 int main(void)
 {
-/*Declare variables*/
+/* Declare variables of different types */
 char c;
 int i;
 long int li;
 long long int lli;
 float f;
 
-/*Print the size of each type using the sizeof operator*/
+/* Print the size of each type using the sizeof operator */
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+/* Return 0 to indicate success */
 return (0);
 }
