1d6bb3f15cd8aad68df624fcdcc98925f4251b42
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-11 05:24:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print a string with proper grammar using printf function
+ * Return 0 to indicatre success
+ */
+int main(void)
+{
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
