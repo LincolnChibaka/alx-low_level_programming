ccf5e0c92ce5721d259151e65693e6afca7ee442
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-14 16:11:40 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,28 +4,28 @@
  * main - Entry point
  *
  * Description: Prints all possible combinations of single-digit numbers.
- *              Numbers are separated by ,, followed by a space.
+ *              Numbers are separated by a comma and a space.
  *              Numbers are printed in ascending order.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int number;
+    int number;
 
-	/* Print possible combinations of single-digit numbers */
-	for (number = 0; number < 10; number++)
-	{
-		putchar('0' + number);
+    /* Print possible combinations of single-digit numbers */
+    for (number = 0; number < 10; number++)
+    {
+        putchar('0' + number);
 
-		if (number < 9)
-		{
-			putchar(',');
-			putchar('	'); /* Use a tab for indentation */
-		}
-	}
+        if (number < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
