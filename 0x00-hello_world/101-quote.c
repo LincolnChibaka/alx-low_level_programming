3b293c5c186c645eb6401bd74210c60eafa3a664
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-14 12:38:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,28 +1,11 @@
 #include <unistd.h>
-#include <string.h>
-#include <sys/syscall.h>
-
-#define STDERR_FILENO 2
-
 /**
- * custom_print - Writes a string to the standard error file descriptor using syscall
- * @str: The string to be printed
- */
-void custom_print(const char *str)
-{
-syscall(SYS_write, STDERR_FILENO, str, strlen(str));
-}
-
-/**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Return: Always 1 (Exit status)
+ * Return: Always 1 (Failure)
  */
 int main(void)
 {
-const char *message =
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-custom_print(message);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
