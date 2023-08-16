05026abee37df653b4880c48332cd0e80f40a16e
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-16 02:25:57 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,50 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* Declare and initialize the variables for the current Fibonacci number */
-	unsigned int f1h = 0;
-	unsigned int f1l = 1;
-	unsigned int f2h = 0;
-	unsigned int f2l = 2;
+	int count = 1; /* Count of Fibonacci numbers (starting with 1 and 2)*/
+	int fib1 = 1;  /*First Fibonacci number*/
+	int fib2 = 2;  /*Second Fibonacci number*/
 
-	/* Declare the variables for the next Fibonacci number */
-	unsigned int nexth, nextl;
+	printf("%d, ", fib1);
+	printf("%d", fib2);
 
-	/* Declare the variable for the carry */
-	unsigned int carry;
-
-	/* Print the first two Fibonacci numbers */
-	printf("%u, %u", f1l, f2l);
-
-	/* Iterate 96 times */
-	int i;
-
-	for (i = 0; i < 96; i++)
+	while (count < 98)
 	{
-		/* Add the current Fibonacci number using the split method */
-		nextl = f1l + f2l;
-		carry = nextl / 1000000;
-		nexth = f1h + f2h + carry;
-		nextl = nextl % 1000000;
+		/* Calculate the next Fibonacci number*/
+		int next_fib = fib1 + fib2;
 
-		/* Update the current Fibonacci number */
-		f1h = f2h;
-		f1l = f2l;
-		f2h = nexth;
-		f2l = nextl;
+		printf(", ");
+		printf("%d", next_fib);
 
-		/* Print the next Fibonacci number with a comma and a space */
-		printf(", %u%06u", nexth, nextl);
+		/*Move the values forward in the sequence*/
+		fib1 = fib2;
+		fib2 = next_fib;
+
+		count++;
 	}
 
-	/* Print a new line */
-	printf("\n");
+	printf("\n"); /*Print a new line at the end*/
+
 	return (0);
 }
-
