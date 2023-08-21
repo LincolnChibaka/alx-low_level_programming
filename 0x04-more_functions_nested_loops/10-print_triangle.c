a45abae0fef683c437c4a2b579a54543b31b41b6
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-21 01:15:46 -0700

    0-reset_to_98.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints a triangle followed by a new line.
- * @size: The size of the triangle.
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: the size of the triangle
  *
- * Return: void.
+ * Return: nothing
  */
 void print_triangle(int size)
 {
-	int a, l, x;
-	if (size <= 0)
+	int i, j, k;
+
+	if (size > 0)
 	{
-		_putchar('\n');
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+            }
+			for (k = 0; k < i; k++)
+			{
+			       	_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 	else
 	{
-	for (int a = 0; a <= size; i++)
-	{
-		for (int l = size - a; l > 1; l--)
-		{
-			_putchar(32);
-		}
-
-		for (int x = 0; x <= a; x++)
-		{
-			_putchar(35);
-		}
-
 		_putchar('\n');
-	}
 	}
 }
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme file connecting description for pointer and arrays project
