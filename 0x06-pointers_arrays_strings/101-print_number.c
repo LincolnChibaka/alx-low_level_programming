f43ca39b225b010b745d3cfcce91fc21497f6729
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-24 06:39:40 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,28 +8,16 @@
  */
 void print_number(int n)
 {
-	int i, j, k, d;
+	unsigned int j = n;
 
-	if (n < 0) /*check if the number is negative*/
+	if (n < 0)
 	{
-		_putchar('-'); /*print a minus sign*/
-		n = -n; /*make the number positive*/
+		_putchar(45);
+		j = -j;
 	}
-
-	i = 1; /*initialize a variable to store the power of 10*/
-	j = n; /*initialize a variable to store the number*/
-
-	while (j > 9) /*loop until the number is less than 10*/
+	if (j / 10)
 	{
-		i = i * 10; /*multiply the power of 10 by 10*/
-		j = j / 10; /*divide the number by 10*/
+		print_number(j / 10);
 	}
-
-	while (i > 0) /*loop until the power of 10 is zero*/
-	{
-		k = n / i; /*get the quotient of the power of 10*/
-		d = k % 10; /* get the remainder of the quotient and 10*/
-		_putchar(d + '0'); /* print the digit as a character*/
-		i = i / 10; /*divide the power of 10 by 10*/
-	}
+	_putchar(j % 10 + '0');
 }
---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point of the program.
+ * It prints a message and returns 0.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
