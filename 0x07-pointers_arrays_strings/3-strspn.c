b417c3740db3026f4005b1bbd9fb90d59fd1f10f
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-28 14:20:39 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,42 +1,30 @@
+#include "main.h"
+
 /**
- * _strspn - Gets the length of a prefix substring.
- * @s: Pointer to the string to search.
- * @accept: Pointer to the string containing acceptable characters.
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to the string
+ * @accept: pointer to the bytes to be accepted
  *
- * Description: This function calculates the length of the initial
- * segment ofstring s that consists only of characters present in
- * the string accept.
- *
- * Return: The number of bytes in the initial segment of s which
- * consist only of bytes from accept.
+ * Return: number of bytes in the initial segment of s which
+ * consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0;
-	int found;
+	unsigned int i, j, count = 0;
 
-	while (*s != '\0')
+	for (i = 0; s[i]; i++)
 	{
-		found = 0;
-
-		for (char *a = accept; *a != '\0'; a++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (*s == *a)
+			if (s[i] == accept[j])
 			{
-				found = 1;
+				count++;
 				break;
 			}
 		}
-
-		if (found == 0)
-		{
-			break;
-		}
-
-		count++;
-		s++;
+		if (!accept[j])
+		break;
 	}
 
 	return (count);
 }
-
