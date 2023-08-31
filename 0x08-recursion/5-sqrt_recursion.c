f60974253008b3c485e98c32f442506986eeb938
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-08-31 05:00:17 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,50 +1,43 @@
 #include "main.h"
 
 /**
- * _sqrt_helper - Helper function to perform binary search for square
- * root.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to compute the square root of
  *
- * @n: The input number for which the square root is to be calculated.
- * @start: The lower bound for binary search.
- * @end: The upper bound for binary search.
- *
- * Return: The natural square root of the number, or -1 if no natural
- * square root.
- */
-int _sqrt_helper(int n, int start, int end)
-{
-	int mid = start + (end - start) / 2;
-
-	if (mid * mid == n)
-		return (mid); /* Square root found */
-
-	if (start >= end)
-		return (-1); /* No natural square root found */
-
-	if (mid * mid > n)
-		/* Search in the left half */
-		return (_sqrt_helper(n, start, mid - 1));
-	/* Search in the right half */
-	return (_sqrt_helper(n, mid + 1, end));
-}
-
-/**
- * _sqrt_recursion - Returns the natural square root of a number.
- *
- * @n: The input number for which the square root is to be calculated.
- *
- * Return: The natural square root of the number, or -1 if no natural square
- * root.
+ * Return: the natural square root of n if it exists, -1 otherwise
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return (-1); /* Return -1 for negative n as error indicator */
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_recursion_helper(n, 0));
+	}
+}
 
-	if (n == 0 || n == 1)
-		/* Base case: square root of 0 or 1 is the number itself */
-		return (n);
-
-	/* Call helper function with initial bounds */
-	return (_sqrt_helper(n, 1, n));
+/**
+ * _sqrt_recursion_helper - helper function for computing the
+ * square root of a number
+ * @n: the number to compute the square root of
+ * @i: the current guess for the square root
+ *
+ * Return: the natural square root of n if it exists, -1 otherwise
+ */
+int _sqrt_recursion_helper(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_recursion_helper(n, i + 1));
+	}
 }
