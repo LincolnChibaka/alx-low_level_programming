d4d271519396b7d34975f17f8e33f3a2d8acbcca
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-09-05 14:24:54 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,34 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * is_space - Checks if a character is a space, tab, or newline.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is a space, tab, or newline; 0 otherwise.
+ */
+int is_space(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+/**
+ * count_words - Counts the number of words in a string.
+ * @str: The input string.
+ *
+ * Return: The number of words in the string.
+ */
+int count_words(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i]; i++)
+		if (!is_space(str[i]) && (is_space(str[i + 1]) || !str[i + 1]))
+			count++;
+
+	return (count);
+}
 
 /**
  * strtow - Splits a string into words.
@@ -10,32 +38,34 @@
  */
 char **strtow(char *str)
 {
-	if (str == NULL || *str == '\0')
+	int i, j, k, len;
+	char **words;
+
+	if (!str || !*str)
 		return (NULL);
 
-	/* Rest of the function... */
-}
+	words = malloc((count_words(str) + 1) * sizeof(char *));
+	if (!words)
+		return (NULL);
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success).
- */
-int main(void)
-{
-	char **result = strtow("  Hello  World  ");
-
-	if (result != NULL)
-	{
-		int i = 0;
-
-		while (result[i] != NULL)
+	for (i = 0, j = 0; str[i]; i++)
+		if (!is_space(str[i]))
 		{
-			printf("%s\n", result[i]);
-			free(result[i]);
-			i++;
+			len = 0;
+			for (k = i; str[k] && !is_space(str[k]); k++)
+				len++;
+
+			words[j] = malloc((len + 1) * sizeof(char));
+			if (!words[j])
+				return (NULL);
+
+			for (k = 0; k < len; k++)
+				words[j][k] = str[i + k];
+			words[j][len] = '\0';
+			j++;
+			i += len;
 		}
-		free(result);
-	}
-	return (0);
+
+	words[j] = NULL;
+	return (words);
 }
