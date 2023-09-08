c66e6a5e08ee740543b12b3ee72d24c6ed4f8f0b
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-09-07 19:28:49 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,54 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * is_positive_integer - Checks if a string is a positive integer.
- * @str: The string to check.
+ * is_digit - checks if a string is composed of digits
+ * @str: string to check
  *
- * Return: 1 if the string is a positive integer, 0 otherwise.
+ * Return: 1 if true, 0 if false
  */
-int is_positive_integer(const char *str)
+int is_digit(char *str)
 {
-	for (int i = 0; str[i] != '\0'; i++)
+	int i; /* loop index */
+
+	/* check each character in the string */
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		/* if not a digit, return 0 */
 		if (!isdigit(str[i]))
-		{
 			return (0);
-		}
 	}
+
+	/* return 1 if all digits */
 	return (1);
 }
 
 /**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 on success, 98 on error.
+ * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, res;
+
+	/* if the number of arguments is incorrect */
 	if (argc != 3)
 	{
+		/* print Error and exit with status 98 */
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 
-	char *num1 = argv[1];
-	char *num2 = argv[2];
+	/* convert the arguments to integers using atoi */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-	if (!is_positive_integer(num1) || !is_positive_integer(num2))
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
 	{
+		/* print Error and exit with status 98 */
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 
-	int int_num1 = atoi(num1);
-	int int_num2 = atoi(num2);
+	/* multiply the two numbers */
+	res = num1 * num2;
 
-	int result = int_num1 * int_num2;
+	/* print the result followed by a new line */
+	printf("%d\n", res);
 
-	printf("%d\n", result);
-
+	/* return 0 on success */
 	return (0);
 }
