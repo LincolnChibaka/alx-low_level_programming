bfde8c74b2cdae28e6e333715d963f1807d3fe5f
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-09-11 11:37:48 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    /* Use the predefined macro __FILE__ to get the file name */
+    printf("%s\n", __FILE__);
+    return (0);
+}
