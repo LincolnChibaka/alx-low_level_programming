d5cd39c0d4c562fbeff9f4c526bc6e58bc7709d2
LincolnChibaka <bac17-lchibaka@poly.ac.mw>
2023-09-11 13:03:37 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /* Define a new type dog_t as a new name for the type struct dog */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
